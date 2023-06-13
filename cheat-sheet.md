a335fba712b7a2b563a09125facd7b31b972b5bb
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-06-13 12:25:11 +0300

    Modified project
    Initialize project and structure
    2. Set up dev enviroment
    3. Install and set up express
        - add static middleware
        - add body parser
        - add routes
    4. Add static resources
    5. Add views folder with ready htmls
    6. Add express-handlebars view engine
        - instal
        - add to express
        - config extentsion
        - add main layout
        - add partials folder
        - render home page
        - fix static paths
        - render home page
    7. Add controllers folder with controller
    8. Connect batabase
        - instal mongoose
        - connect
    9. Authentication
        - add user controller
        - add controller to routes
        - render login page

---
+++ cheat-sheet.md
@@ -0,0 +1,27 @@
+# Cheat Sheet
+
+1. Initialize project and structure
+2. Set up dev enviroment
+3. Install and set up express
+    - add static middleware
+    - add body parser
+    - add routes
+4. Add static resources
+5. Add views folder with ready htmls
+6. Add express-handlebars view engine
+    - instal
+    - add to express
+    - config extentsion
+    - add main layout
+    - add partials folder
+    - render home page
+    - fix static paths
+    - render home page
+7. Add controllers folder with controller
+8. Connect batabase
+    - instal mongoose
+    - connect
+9. Authentication
+    - add user controller
+    - add controller to routes
+    - render login page
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+const PORT = 5000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/petstagram')
+.then(() => console.log('DB connected successfully!'))
+.catch(err => console.log('DB error', err.message));
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
+app.use(routes);
+
+app.listen(5000, console.log(`Server is listening on port ${PORT}...`));
