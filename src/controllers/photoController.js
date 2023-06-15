88a6b695453a45d7f300efb3eec1e7f30e7af5f9
Ceci Popova <97601664+CeciPopova@users.noreply.github.com>
2023-06-15 23:24:43 +0300

    Add comments

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -32,7 +32,7 @@ router.post('/create', async (req, res) => {
 
 router.get('/:photoId/details', async (req, res) => {
     const photoId = req.params.photoId;
-    const photo = await photoManager.getOne(photoId).lean();
+    const photo = await photoManager.getOne(photoId).populate('comments.user').lean();
     const isOwner = req.user?._id == photo.owner._id;
 
     res.render('photos/details', { photo, isOwner });
@@ -48,7 +48,6 @@ router.get('/:photoId/delete', async (req, res) => {
     } catch (err) {
         res.render(`/photos/details`, { error: 'Unsuccessful photo deletion!' });
     }
-
 });
 
 router.get('/:photoId/edit', async (req, res) => {
@@ -66,7 +65,21 @@ router.post('/:photoId/edit', async (req, res) => {
     } catch (err) {
         res.render('photos/edit', { error: 'Unable to update photo', ...photoData });
     }
-
 });
+
+router.post('/:photoId/comments', async (req, res) => {
+    const photoId = req.params.photoId;
+
+    const { message } = req.body;
+    const user = req.user._id;
+
+    try {
+        await photoManager.addComment(photoId, { user, message });
+
+        res.redirect(`/photos/${photoId}/details`);
+    } catch (error) {
+        
+    }
+})
 
 module.exports = router;
